a77abea402c0ee0112ec4690e763c126d073131b
Julia <Julia@MacBook-Pro-Annet.local>
2022-09-08 13:04:32 +0300

    created blocks with text main

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/chevronleft.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/chevronright.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/chevronleft.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/chevronright.png"></button>',
         responsive: [{
             breakpoint: 992,
             settings: {
